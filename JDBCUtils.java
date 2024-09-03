28c3b1a781daa123daecf8858d876cf9f9f3f103
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-03 22:05:42 +0530

    Add files via upload

---
+++ JDBCUtils.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JDBCUtils {
+
+    public static Connection getConnection() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getConnection'");
+    }
+
+    public static void printSQLException(SQLException e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printSQLException'");
+    }
+
+}
