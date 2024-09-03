25e5d96e8145a259e02490d103bcb06788f87647
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-09-03 22:05:11 +0530

    Add files via upload

---
+++ connection.java
@@ -0,0 +1,28 @@
+package bank;
+import java.sql.Connection;
+import java.sql.DriverManager;
+// Global connection Class
+public class connection {
+	static Connection con =null; // Global Connection Object
+	public static Connection getConnection()
+	{
+		try {
+			
+			
+			String mysqlJDBCDriver
+				= "com.mysql.cj.jdbc.Driver"; //jdbc driver
+			String url
+				= "jdbc:mysql://localhost:3306/bank"; //mysql url
+			String user = "root";	 //mysql username
+			String pass = "1998"; //mysql passcode
+			Class.forName(mysqlJDBCDriver);
+			con = DriverManager.getConnection(url, user, pass);
+		}
+		catch (Exception e) {
+			System.out.println("Connection Failed!");
+		}
+
+		return con;
+	}
+}
+ 
